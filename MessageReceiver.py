96c2d0c35aae1451af317a6be2ea33b687541cfd
Trine Marie Tonhaugen <trinm@online.no>
2016-03-08 22:31:08 +0100

    MessageReceiver fra Trine

---
+++ MessageReceiver.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from threading import Thread
+
+BUFFER_SIZE = 1024
+
+class MessageReceiver(Thread):
+    """
+    This is the message receiver class. The class inherits Thread, something that
+    is necessary to make the MessageReceiver start a new thread, and it allows
+    the chat client to both send and receive messages at the same time
+    """
+
+    def __init__(self, client, connection):
+        """
+        This method is executed when creating a new MessageReceiver object
+        """
+
+        # Flag to run thread as a deamon
+        self.daemon = True
+
+        # TODO: Finish initialization of MessageReceiver
+        self.client = client
+        self.connection = connection
+
+    def run(self):
+        # TODO: Make MessageReceiver receive and handle payloads
+
+        while 1:
+            data = self.conn.recv(BUFFER_SIZE)
+            if not data: 
+                break
+            self.client.receive_message(data)
